1d8de7b9ab283bfc896e549792160699702addeb
Cameron Houck <cameronhouck1@gmail.com>
2021-07-12 17:56:25 -0400

    adding install function

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,8 +7,18 @@ const FILES_TO_CACHE = [
   "/assets/css/styles.css",
   "/assets/js/db.js",
   "/assets/js/index.js",
-  '/service-worker.js',
+  "/service-worker.js",
   "/manifest.webmanifest",
   "/assets/icons/icon-192x192.png",
   "/assets/icons/icon-512x512.png",
 ];
+
+self.addEventListener("install", function (e) {
+  ev.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
