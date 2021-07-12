659076bb6705944c8ae7837c8d8712e51f04e9dc
Cameron Houck <cameronhouck1@gmail.com>
2021-07-12 12:59:10 -0400

    adding files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/assets/css/styles.css",
+  "/assets/js/db.js",
+  "/assets/js/index.js",
+  '/service-worker.js',
+  "/manifest.webmanifest",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-512x512.png",
+];
