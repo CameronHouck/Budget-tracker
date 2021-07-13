5360b43b58be1f3d900c701bbb2d91e5fbc92e91
Cameron Houck <cameronhouck1@gmail.com>
2021-07-13 11:20:29 -0400

    adding check database and error function

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -15,3 +15,30 @@ function saveRecord(record) {
   const store = transaction.objectStore("budgetStore");
   store.add(record);
 }
+
+function checkDatabase() {
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["budgetStore"], "readwrite");
+          const store = transaction.objectStore("budgetStore");
+          store.clear();
+        });
+    }
+  };
+}
+
+request.onerror = function (e) {
+  console.log("Whoops!" + e.target.errorCode);
+};
+
+window.addEventListener("online", checkDatabase);
