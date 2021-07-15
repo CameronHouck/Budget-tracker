7beb958bd1060acf01e2c6a2d6294dd7ac3c53f6
Cameron Houck <cameronhouck1@gmail.com>
2021-07-15 17:18:34 -0400

    fixing database error

--- routes/public/assets/js/db.js
+++ routes/public/assets/js/db.js
@@ -1,13 +1,21 @@
 let db;
-
 const request = indexedDB.open("budget", 1);
 
-request.onsuccess = function (e) {
-  db = e.target.result;
+request.onupgradeneeded = function (ev) {
+  const db = ev.target.result;
+  db.createObjectStore("budgetStore", { autoIncrement: true });
+};
+
+request.onsuccess = function (ev) {
+  db = ev.target.result;
   if (navigator.onLine) {
-    console.log("Backend Online!");
+    console.log("Backend is online!");
     checkDatabase();
   }
+};
+
+request.onerror = function (ev) {
+  console.log("Uh-Oh!" + ev.target.errorCode);
 };
 
 function saveRecord(record) {
@@ -17,6 +25,9 @@ function saveRecord(record) {
 }
 
 function checkDatabase() {
+  const transaction = db.transaction(["budgetStore"], "readwrite");
+  const store = transaction.objectStore("budgetStore");
+  const getAll = store.getAll();
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
@@ -36,9 +47,5 @@ function checkDatabase() {
     }
   };
 }
-
-request.onerror = function (e) {
-  console.log("Whoops!" + e.target.errorCode);
-};
 
 window.addEventListener("online", checkDatabase);
